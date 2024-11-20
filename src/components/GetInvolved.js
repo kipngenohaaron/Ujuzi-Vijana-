84056e7eeaecd6f230a7b40c3e6ed767558b834a
kipngenohaaron <kipngenohaaron@gmail.com>
2024-11-20 15:49:12 +0300

    get involve section

---
+++ src/components/GetInvolved.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function GetInvolved() {
+  return (
+    <section id="get-involved">
+      <h2>Get Involved</h2>
+      <div className="cta">
+        <a href="#volunteer" className="cta-button">Volunteer</a>
+        <a href="#donate" className="cta-button">Donate Now</a>
+      </div>
+    </section>
+  );
+}
+
+export default GetInvolved;
