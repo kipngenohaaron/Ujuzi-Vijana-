8d301562fd2c3c558a13a363a296e5b548f7fb8a
kipngenohaaron <kipngenohaaron@gmail.com>
2024-11-20 17:09:03 +0300

    Rpouting

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import React from "react";
-import Header from "./components/Header";
-import Hero from "./components/Hero";
-import About from "./components/About";
-import Programs from "./components/Programs";
-import GetInvolved from "./components/GetInvolved";
-import Footer from "./components/Footer";
-import Contact from "./components/Contact";
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import React from "react";
+// import Header from "./components/Header";
+// import Hero from "./components/Hero";
+// import About from "./components/About";
+// import Programs from "./components/Programs";
+// import GetInvolved from "./components/GetInvolved";
+// import Footer from "./components/Footer";
+// import Contact from "./components/Contact";
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+// function App() {
+//   return (
+//     <div>
+//       <Header />
+//       <Hero />
+//       <About />
+//       <Programs />
+//       <GetInvolved />
+//       <Contact />
+//       <Footer />
+//     </div>
+//   );
+// }
+
+// export default App;
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import About from './components/About';
+import Programs from './components/Programs';
+import GetInvolved from './components/GetInvolved';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import './styles.css'; // Import global styles
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Hero />
-      <About />
-      <Programs />
-      <GetInvolved />
-      <Contact />
-      <Footer />
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Hero />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/programs" element={<Programs />} />
+          <Route path="/get-involved" element={<GetInvolved />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
