db2939923c8355aa843958d8a6ad5f1c1934ad6c
kipngenohaaron <kipngenohaaron@gmail.com>
2024-11-20 15:49:56 +0300

    footer added

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <div className="footer-links">
+        <a href="#contact">Contact Us</a>
+        <a href="#privacy">Privacy Policy</a>
+        <a href="#terms">Terms of Use</a>
+      </div>
+      <div className="social-media">
+        <a href="https://facebook.com/ujuzivijana">Facebook</a>
+        <a href="https://instagram.com/ujuzivijana">Instagram</a>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
