30786cd21674e624bd27bdaf4a29c65abac5830a
kipngenohaaron <kipngenohaaron@gmail.com>
2024-11-20 17:30:51 +0300

    App,js modified

--- src/App.js
+++ src/App.js
@@ -1,28 +1,3 @@
-// import React from "react";
-// import Header from "./components/Header";
-// import Hero from "./components/Hero";
-// import About from "./components/About";
-// import Programs from "./components/Programs";
-// import GetInvolved from "./components/GetInvolved";
-// import Footer from "./components/Footer";
-// import Contact from "./components/Contact";
-// import 'bootstrap/dist/css/bootstrap.min.css';
-
-// function App() {
-//   return (
-//     <div>
-//       <Header />
-//       <Hero />
-//       <About />
-//       <Programs />
-//       <GetInvolved />
-//       <Contact />
-//       <Footer />
-//     </div>
-//   );
-// }
-
-// export default App;
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
