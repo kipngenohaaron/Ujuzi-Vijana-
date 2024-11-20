2d3b95cddb7b2e1e78ae9c02858889fc29a110ac
kipngenohaaron <kipngenohaaron@gmail.com>
2024-11-20 17:33:07 +0300

    footer details added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,32 +1,16 @@
-// import React from "react";
-
-// function Footer() {
-//   return (
-//     <footer>
-//       <div className="footer-links">
-//         <a href="#contact">Contact Us</a>
-//         <a href="#privacy">Privacy Policy</a>
-//         <a href="#terms">Terms of Use</a>
-//       </div>
-//       <div className="social-media">
-//         <a href="https://facebook.com/ujuzivijana">Facebook</a>
-//         <a href="https://instagram.com/ujuzivijana">Instagram</a>
-//       </div>
-//     </footer>
-//   );
-// }
-
-// export default Footer;
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Footer() {
   return (
     <footer className="bg-light text-center py-4">
       <p>© 2024 Ujuzi Vijana | All Rights Reserved</p>
       <div>
-        <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">Facebook</a> | 
-        <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">Instagram</a> | 
-        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">Twitter</a>
+        <Link to="/get-involved" className="me-3">Get Involved</Link>
+        <Link to="/contact" className="me-3">Contact Us</Link>
+        <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" className="me-3">Facebook</a>
+        <a href="https://instagram.com" target="_blank" rel="noopener noreferrer" className="me-3">Instagram</a>
+        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" className="me-3">Twitter</a>
       </div>
     </footer>
   );
