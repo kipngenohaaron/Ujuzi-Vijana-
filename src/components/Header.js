b3ca0e9355a2099681583484a48cd2648bb97ef7
kipngenohaaron <kipngenohaaron@gmail.com>
2024-11-21 17:08:58 +0300

    styling input on transformation

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,9 +5,9 @@ function Header() {
   const location = useLocation();
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-light">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container">
-        <Link to="/" className="navbar-brand logo">
+        <Link to="/" className="navbar-brand">
           Ujuzi Vijana
         </Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
